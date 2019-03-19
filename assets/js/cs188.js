88f5807a304393ab7957ff824916bdb2fd63f905
yuhongc <mikechen212@gmail.com>
2019-03-19 15:30:10 -0700

    update style

--- assets/js/cs188.js
+++ assets/js/cs188.js
@@ -4,3 +4,10 @@ $(function () {
         div.fadeToggle(1000);
     });
 });
+
+$(function () {
+    let div = $('#entailment-eg');
+    $('#show-entailment-eg').click(function () {
+        div.fadeToggle(1000);
+    });
+});

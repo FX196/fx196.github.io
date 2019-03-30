a7aad4d57fd97ebb2eeb839ae5f5a437976fd094
yuhongc <mikechen212@gmail.com>
2019-03-30 22:51:45 +0000

    add processing files

---
+++ Processing/js/blobby.js
@@ -0,0 +1,29 @@
+let noiseMax = 2;
+let t = 0;
+let phase = 0;
+let phase_delta = 0.00001;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+    translate(width / 2, height / 2);
+    stroke(255);
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += 0.01) {
+        let xOff = map(cos(a), -1, 1, 0, noiseMax);
+        let yOff = map(sin(a), -1, 1, 0, noiseMax);
+        let r = map(noise(xOff, yOff, t), 0, 1, 120, 160);
+        let x = r * cos(a + phase);
+        let y = r * sin(a + phase);
+        vertex(x, y);
+    }
+    if (random(1000) <= 10) {
+        phase_delta = -phase_delta;
+    }
+    phase += phase_delta;
+    t += 0.01;
+    endShape(CLOSE);
+}

2eb1be3d91b5a71a31aa6977ce6aeaa3ba5940bc
yuhongc <mikechen212@gmail.com>
2019-05-13 14:10:07 -0700

    update steaming memory

--- assets/js/cs170.js
+++ assets/js/cs170.js
@@ -4,3 +4,10 @@ $(function () {
         div.fadeToggle(1000);
     });
 });
+
+$(function () {
+    let div = $('#memory-bound-proof');
+    $('#show-memory-bound-proof').click(function () {
+        div.fadeToggle(1000);
+    });
+});
